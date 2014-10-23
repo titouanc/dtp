a27e0eddecfc93a4a1b499ee326965de67f39187
jschembr <jschembr@ulb.ac.be>
2014-10-23 16:54:37 +0200

    [add] GUI first class 2

---
+++ src/be/ac/ulb/infof307/g03/GUI/GUI.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package be.ac.ulb.infof307.g03.GUI;
+
+import javax.swing.*;
+
+/**
+ * @author julianschembri
+ *
+ */
+public class GUI {
+	public GUI() {
+		System.out.println("Hello world!");
+	}
+}
