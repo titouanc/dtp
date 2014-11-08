44fda731183598cc027c08aaf139bfe60b59d502
Titouan Christophe <titouan.christophe@ulb.ac.be>
2014-11-08 14:17:24 +0100

    [enh] Remove WorldView.createDemoProject

--- src/be/ac/ulb/infof307/g03/views/WorldView.java
+++ src/be/ac/ulb/infof307/g03/views/WorldView.java
@@ -30,7 +30,7 @@ import com.jme3.util.SkyFactory;
 
 /**
  * This class is a jMonkey canvas that can be added in a Swing GUI.
- * @author fhennecker, julianschembri, brochape
+ * @author fhennecker, julianschembri, brochape, Titouan
  */
 public class WorldView extends SimpleApplication implements Observer {	
 	
@@ -65,79 +65,16 @@ public class WorldView extends SimpleApplication implements Observer {
 		//Generate the axes
 		attachAxes();
 		
-		//Sets up the demo project
-		createDemoGeometry();
-		
 		//Change the default background
 		viewPort.setBackgroundColor(ColorRGBA.White);
+		
+		//render the scene
 		_makeScene();
+		
 		// Notify our controller that initialisation is done
 		_controller.onViewCreated();
 	}
 
-	
-	/**
-	 * Creates a world demo
-	 */
-	public void createDemoGeometry() {
-		
-		Vector3f [] vertices = new Vector3f[8];
-		vertices[0] = new Vector3f(0,0,0);
-		vertices[1] = new Vector3f(-1,1,0);
-		vertices[2] = new Vector3f(0,6,0);
-		vertices[3] = new Vector3f(3,5,0);
-		vertices[4] = new Vector3f(0,0,2);
-		vertices[5] = new Vector3f(-1,1,2);
-		vertices[6] = new Vector3f(0,6,2);
-		vertices[7] = new Vector3f(3,5,2);
-
-
-		int n = 4;
-		int[] order = new int[(n-1)*2*3];//4 points -> 3 walls; each wall -> 2 triangles; each triangle ->3 points
-		int i;
-		for(i=0;i<(n-1);++i){
-			order[6*i] = i;
-			order[6*i+1] = i+1;
-			order[6*i+2] = i+n;
-			order[6*i+3] = i+1;
-			order[6*i+4] = i+n+1;
-			order[6*i+5] = i+n;
-		}
-
-		Mesh wallsMesh = new Mesh();
-		wallsMesh.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
-		wallsMesh.setBuffer(Type.Index,    3, BufferUtils.createIntBuffer(order));
-		wallsMesh.updateBound();
-
-		int[] groundOrder = new int[(n-2)*3];
-
-		for(int j = 0; j< n-2;++j){
-			groundOrder[3*j] = 0;
-			groundOrder[3*j+1] = j+2;
-			groundOrder[3*j+2] = j+1;      
-		}
-
-		Mesh groundMesh = new Mesh();
-		groundMesh.setBuffer(Type.Position, 3, BufferUtils.createFloatBuffer(vertices));
-		groundMesh.setBuffer(Type.Index, 3, BufferUtils.createIntBuffer(groundOrder));
-
-		Geometry walls = new Geometry("Walls",wallsMesh);
-		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
-		mat.setColor("Color", ColorRGBA.Gray);
-		walls.setMaterial(mat);
-		rootNode.attachChild(walls);
-		
-		Geometry ground = new Geometry("Groundmesh",groundMesh);
-		Material mat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-		mat2.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
-		mat2.setColor("Color", ColorRGBA.LightGray);
-		ground.setMaterial(mat2);
-		shapes.add(ground);
-		rootNode.attachChild(ground);
-		
-		
-	}
 	/**
 	 * This methods created the grid and adds it to the background
 	 */
