29948226707489e83f0fe4ddbc508361d090c26a
Pierre Gerard <piegerar@ulb.ac.be>
2014-11-16 20:45:20 +0100

    [fix] Bootcontroller test

--- test/be/ac/ulb/infof307/g03/controllers/TestBootController.java
+++ test/be/ac/ulb/infof307/g03/controllers/TestBootController.java
@@ -1,6 +1,11 @@
 package be.ac.ulb.infof307.g03.controllers;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import be.ac.ulb.infof307.g03.models.Project;
 
 
 /**
@@ -9,12 +14,27 @@ import org.junit.Before;
  */
 public class TestBootController {
 	
+	private BootController _controller;
+	private static final String RANDOM_PATH = "/home/tester/myproject.hpj";
+	//private Project _project;
+	
+	
+	/**
+	 * 
+	 */
 	@Before
 	public void setUp(){
+		_controller = new BootController();
+		_controller.saveCurrentProjectPath(RANDOM_PATH);
 		
 	}
 	
+	/**
+	 * 
+	 */
+	@Test
 	public void testCreation(){
+		assertEquals(_controller.getLastProjectPath(), RANDOM_PATH);
 		
 	}
 
