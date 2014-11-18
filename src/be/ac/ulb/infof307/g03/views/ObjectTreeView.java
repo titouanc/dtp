0990a3a49c9102acf9c9a9511203bceaae83f63b
Titouan Christophe <titouan.christophe@ulb.ac.be>
2014-11-18 14:07:13 +0100

    [ref] Remove GeometricTree, build the TreeView ourselves (nearly working !)

--- src/be/ac/ulb/infof307/g03/views/ObjectTreeView.java
+++ src/be/ac/ulb/infof307/g03/views/ObjectTreeView.java
@@ -11,6 +11,11 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -20,6 +25,7 @@ import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
@@ -31,14 +37,16 @@ import be.ac.ulb.infof307.g03.models.*;
  * @author pierre, titou
  * 
  */
-public class ObjectTreeView extends JPanel implements TreeSelectionListener {
+public class ObjectTreeView extends JPanel implements TreeSelectionListener, Observer {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTree _tree;
-	private GeometricTree _model;
 	private ObjectTreeController _controller;
+	private GeometryDAO _dao;
+	private DefaultMutableTreeNode _root = new DefaultMutableTreeNode("Root");
+	private Map<String,DefaultMutableTreeNode> _nodes = new HashMap<String,DefaultMutableTreeNode>();
 	
 	private JPopupMenu _popupMenuRD;
 	private JPopupMenu _popupMenuD;
@@ -59,13 +67,14 @@ public class ObjectTreeView extends JPanel implements TreeSelectionListener {
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			String cmd = event.getActionCommand();
+			DefaultMutableTreeNode clickedNode = (DefaultMutableTreeNode) _tree.getLastSelectedPathComponent();
+	        Geometric clickedItem = (Geometric) clickedNode.getUserObject();
 			if (cmd.equals(_RENAME)) {
 				String name = JOptionPane.showInputDialog("New name ?");
-				_controller.renameNode(_tree.getLastSelectedPathComponent(), name);
+				_controller.renameNode(clickedItem, name);
 			} else if (cmd.equals(_DELETE)) {
-				System.out.println("[DEBUG] User clicked on delete");
-				_controller.deleteNode(_tree.getLastSelectedPathComponent());
-			
+				_controller.deselectElement(clickedItem);
+				_controller.deleteNode(clickedItem);
 			}
 		}
 
@@ -92,9 +101,38 @@ public class ObjectTreeView extends JPanel implements TreeSelectionListener {
 			if (value instanceof Grouped){
 				Grouped item = (Grouped) value;
 				sel = item.isSelected();
+				System.out.println("[RENDER TREE ELEMENT] " + item.toString());
 			}
 			super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
 			return this;
+		}
+	}
+	
+	private DefaultMutableTreeNode _createNode(Geometric item){
+		DefaultMutableTreeNode res = new DefaultMutableTreeNode(item.toString());
+		res.setUserObject(item);
+		res.setAllowsChildren(! item.isLeaf());
+		_nodes.put(item.getUID(), res);
+		return res;
+	}
+	
+	private DefaultMutableTreeNode _createTree(Geometric root) throws SQLException{
+		DefaultMutableTreeNode res = _createNode(root);
+		if (root instanceof Group){
+			for (Grouped grouped : _dao.getGrouped((Group) root))
+				res.add(_createNode(grouped));
+			for (Shape shape : _dao.getShapesForGroup((Group) root))
+				res.add(_createTree(shape));
+		}
+		return res;
+	}
+	
+	private void _createTree() throws SQLException{
+		for (Floor floor : _dao.getFloors()){
+			DefaultMutableTreeNode floorNode = _createNode(floor);
+			for (Group group : _dao.getGroups(floor))
+				floorNode.add(_createTree(group));
+			_root.add(floorNode);
 		}
 	}
 	
@@ -106,20 +144,22 @@ public class ObjectTreeView extends JPanel implements TreeSelectionListener {
 		_controller = newController;
 		
 		try {
-			_model = new GeometricTree(project.getGeometryDAO());
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			_dao = project.getGeometryDAO();
+			_createTree();
+			_dao.addObserver(this);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
 		}
-
+		
 		// Create a tree that allows one selection at a time.
-		_tree = new JTree(_model);
+		_tree = new JTree(_root);
 		_tree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
 		_tree.setCellRenderer(new GeometricRenderer());
 		
 		// Listen for when the selection changes
 		_tree.addTreeSelectionListener(this);
 		_tree.setRootVisible(false);
+		_tree.setShowsRootHandles(true);
 		
 		// Add a menu popup to the tree
 		_popupMenuRD = new JPopupMenu();
@@ -149,13 +189,14 @@ public class ObjectTreeView extends JPanel implements TreeSelectionListener {
 	    		 	// select the closest element near the click on the tree
 	    	        int row = _tree.getClosestRowForLocation(e.getX(), e.getY());
 	    	        _tree.setSelectionRow(row);
-	    	        if (_tree.getLastSelectedPathComponent() instanceof Group) {
+	    	        DefaultMutableTreeNode clickedNode = (DefaultMutableTreeNode) _tree.getLastSelectedPathComponent();
+	    	        Geometric clickedItem = (Geometric) clickedNode.getUserObject();
+	    	        if (clickedItem instanceof Group) {
 	    	        	 _popupMenuRD.show(e.getComponent(), e.getX(), e.getY());
 	    	        }
-	    	        else if ((_tree.getLastSelectedPathComponent() instanceof Wall) || (_tree.getLastSelectedPathComponent() instanceof Ground)) {
+	    	        else if (clickedItem instanceof Grouped) {
 	    	        	 _popupMenuD.show(e.getComponent(), e.getX(), e.getY());
 	    	        }
-	    	        //_popupMenuRD.show(e.getComponent(), e.getX(), e.getY());
 	    	    }
 		     }
 		 };
@@ -167,13 +208,61 @@ public class ObjectTreeView extends JPanel implements TreeSelectionListener {
 		add(_tree);
 	}
 	
+	private Geometric _getGeometric(TreePath path){
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) path.getLastPathComponent();
+		return (Geometric) node.getUserObject();
+	}
+	
 	@Override
 	public void valueChanged(TreeSelectionEvent event) {
 		TreePath path = event.getOldLeadSelectionPath();
 		if (path != null)
-			_controller.deselectElement(path.getLastPathComponent());
+			_controller.deselectElement(_getGeometric(path));
 		path = event.getNewLeadSelectionPath();
 		if (path != null)
-			_controller.selectElement(path.getLastPathComponent());
+			_controller.selectElement(_getGeometric(path));
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		boolean updateUI = false;
+		List<Change> changes = (List<Change>) arg1;
+		for (Change change : changes){
+			Geometric changed = change.getItem();
+			if (change.isUpdate() && changed instanceof Group){
+				// Update grouped (name might have changed)
+			} else if (change.isDeletion()){
+				DefaultMutableTreeNode node = _nodes.get(changed.getUID());
+				if (node != null){
+					node.removeFromParent();
+					_nodes.remove(node);
+					updateUI = true;
+				}
+			} else if (change.isCreation() && ! _nodes.containsKey(changed.getUID())){
+				DefaultMutableTreeNode newNode = null;
+				try {newNode = _createTree(changed);}
+				catch (SQLException err){err.printStackTrace();}
+				
+				if (changed instanceof Floor){
+					_root.add(newNode);
+					updateUI = true;
+				} else if (changed instanceof Grouped){
+					_nodes.get(((Grouped) changed).getGroup().getUID()).add(newNode);
+					updateUI = true;
+				} else if (changed instanceof Group){
+					Group grp = (Group) changed;
+					if (grp.getGroup() != null)
+						_nodes.get(grp.getGroup().getUID()).add(newNode);
+					else if (grp.getFloor() != null)
+						_nodes.get(grp.getFloor().getUID()).add(newNode);
+					updateUI = true;
+				}
+				
+			}
+		}
+		if (updateUI){
+			_tree.updateUI();
+			System.out.println(changes);
+		}
 	}
 }
