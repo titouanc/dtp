f85bdfd7f544961b1a28c14174129be6df4dcda1
brochape <brochape@ulb.ac.be>
2014-11-27 18:20:03 +0100

    [add] Added the panes for the colors and textures

--- src/be/ac/ulb/infof307/g03/views/TextureView.java
+++ src/be/ac/ulb/infof307/g03/views/TextureView.java
@@ -3,7 +3,11 @@ package be.ac.ulb.infof307.g03.views;
 import javax.swing.ButtonGroup;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
@@ -15,6 +19,9 @@ import be.ac.ulb.infof307.g03.models.Project;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -24,45 +31,73 @@ import java.util.Observer;
  * @author Walter
  * @brief view of the panel that ill open when user wants to change texture
  */
-public class TextureView extends JToolBar implements Observer {
+public class TextureView extends JPanel implements Observer {
 
 	private static final long serialVersionUID = 1L;
 	private TextureController _textureController;
 	private Project _project;
+	
+	private JPanel _typeSelection;
+	private JScrollPane _materialList;
+	private GridBagLayout _paneLayout;
+	
 
 	/**
 	 * @param newControler
 	 * @param project
 	 */
 	public TextureView(TextureController newControler, Project project) {
-    	super("Texture Edit",JToolBar.VERTICAL);  	
+    	super();  	
     	_textureController = newControler;
     	_project = project;
-        this.setFloatable(false); 
         
-        String prefix = System.getProperty("user.dir") + "/src/be/ac/ulb/infof307/g03/asset/";
-		ImageIcon redIcon = new ImageIcon(prefix + "cursor.png");
+    	_paneLayout = new GridBagLayout();
+    	//this.setLayout(_paneLayout);
+    	this.setLayout(_paneLayout);
 		
-		//ButtonGroup buttonGroup = new ButtonGroup();
-		
-		
-		this.setLayout(new FlowLayout(FlowLayout.TRAILING));
-        
-        JToggleButton redButton = new JToggleButton("Red",redIcon);
-        redButton.setSize(this.getWidth(), 50);
-        //redButton.setSize(50, 50);
-
-        //buttonGroup.add(redButton);
-        
-        JToggleButton greenButton = new JToggleButton("Green",redIcon);
-        greenButton.setSize(50, 50);
-        //buttonGroup.add(greenButton);
-        //buttonGroup.
-        this.add(redButton);
-        this.add(greenButton);
+		this.addTypeSelection();
+		this.addMaterialChoice();
         
     }
 	
+	public void addTypeSelection(){
+		_typeSelection = new JPanel();
+
+        GridBagConstraints c = new GridBagConstraints();
+		
+		JLabel typeLabel = new JLabel("Type : ");
+		JComboBox<String> typeChoice = new JComboBox<String>();
+		typeChoice.addItem("Color");
+		typeChoice.addItem("Texture");
+		
+		_typeSelection.add(typeLabel);
+		_typeSelection.add(typeChoice);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+	    c.anchor = GridBagConstraints.PAGE_START; //bottom of space		
+		this.add(_typeSelection,c);
+		
+	}
+	
+	public void addMaterialChoice(){
+
+
+		String[] data = {"one", "two", "three", "four"};
+		JList<String> materialList = new JList<String>(data);
+		_materialList = new JScrollPane(materialList);
+
+        GridBagConstraints c = new GridBagConstraints();
+
+		c.fill = GridBagConstraints.BOTH;
+	    c.anchor = GridBagConstraints.PAGE_START; //bottom of space;
+	    c.weightx = 1;
+	    c.weighty = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.gridheight = 2;
+		//c.ipady = 200;
+		this.add(_materialList,c);
+	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
