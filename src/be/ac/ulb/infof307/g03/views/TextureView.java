ec7684aebeb0aca14ce24b03ce242b844e2f267f
brochape <brochape@ulb.ac.be>
2014-11-27 17:03:32 +0100

    [add] Added buttons in texture pane

--- src/be/ac/ulb/infof307/g03/views/TextureView.java
+++ src/be/ac/ulb/infof307/g03/views/TextureView.java
@@ -14,6 +14,7 @@ import be.ac.ulb.infof307.g03.models.Config;
 import be.ac.ulb.infof307.g03.models.Project;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -34,16 +35,34 @@ public class TextureView extends JToolBar implements Observer {
 	 * @param project
 	 */
 	public TextureView(TextureController newControler, Project project) {
-    	super("Texture Edit");  	
+    	super("Texture Edit",JToolBar.VERTICAL);  	
     	_textureController = newControler;
     	_project = project;
         this.setFloatable(false); 
-        JPanel contentPane = new JPanel(new BorderLayout());    
-        // Create the toolbar
-        //_textureController = new TextureController(project);
-        //_textureController.run();
+        
+        String prefix = System.getProperty("user.dir") + "/src/be/ac/ulb/infof307/g03/asset/";
+		ImageIcon redIcon = new ImageIcon(prefix + "cursor.png");
+		
+		//ButtonGroup buttonGroup = new ButtonGroup();
+		
+		
+		this.setLayout(new FlowLayout(FlowLayout.TRAILING));
+        
+        JToggleButton redButton = new JToggleButton("Red",redIcon);
+        redButton.setSize(this.getWidth(), 50);
+        //redButton.setSize(50, 50);
+
+        //buttonGroup.add(redButton);
+        
+        JToggleButton greenButton = new JToggleButton("Green",redIcon);
+        greenButton.setSize(50, 50);
+        //buttonGroup.add(greenButton);
+        //buttonGroup.
+        this.add(redButton);
+        this.add(greenButton);
         
     }
+	
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
