cd79fcc0a57daebed7cd448314c40f107207b1d5
Pierre Gerard <piegerar@ulb.ac.be>
2014-11-29 16:12:18 +0100

    [enh] first first floor of a house

--- src/be/ac/ulb/infof307/g03/models/DemoProject.java
+++ src/be/ac/ulb/infof307/g03/models/DemoProject.java
@@ -24,16 +24,26 @@ public class DemoProject {
 		
 		GeometryDAO geo = proj.getGeometryDAO();
 		Point a = new Point(0, 0, 0),
-			  b = new Point(3, 0, 0),
-			  c = new Point(7, 8, 0),
-			  d = new Point(0, 12, 0),
-			  e = new Point(-5, -1, 0);
+			  b = new Point(8, 0, 0),
+			  c = new Point(8, 8, 0),
+			  d = new Point(0, 8, 0),
+			  e = new Point(-8, 0, 0),
+			  f = new Point(-8, 8, 0),
+			  g = new Point(8, -2, 0),
+			  h = new Point(0, -2, 0),
+			  i = new Point(4, -4.5, 0),
+			  j = new Point(2, -4, 0),
+			  k = new Point(6, -4, 0),
+			  l = new Point(8, 14, 0),
+			  m = new Point(-8, 14, 0);
 		
 		Floor groundFloor = geo.getFloors().get(0);
 		proj.config("floor.current", groundFloor.getUID());
 		
-		createRoom(groundFloor, "Irregular room", a, b, c, d);
-		createRoom(groundFloor, "Triangular room", a, e, d);
+		createRoom(groundFloor, "Square room", a, e, f, d);
+		createRoom(groundFloor, "Irregular room", a, d, c, c, g,k, i,j, h);
+		createRoom(groundFloor, "Rectangular room", f, d, c, l, m);
+		//createRoom(groundFloor, "Irregular room", g,k, i,j, h);
 		geo.notifyObservers();
 		return proj;
 	}
