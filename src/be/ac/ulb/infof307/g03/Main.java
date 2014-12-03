9e244b0aaf679b6fe859e622ec498ca258ea0b03
Pierre Gerard <piegerar@ulb.ac.be>
2014-12-03 16:40:20 +0100

    [add] splashscreen class

--- src/be/ac/ulb/infof307/g03/Main.java
+++ src/be/ac/ulb/infof307/g03/Main.java
@@ -3,11 +3,14 @@ package be.ac.ulb.infof307.g03;
 import java.sql.SQLException;
 import java.util.logging.Level;
 
+import javax.swing.ImageIcon;
+
 import com.j256.ormlite.logger.LocalLog;
 
 import be.ac.ulb.infof307.g03.controllers.BootController;
 import be.ac.ulb.infof307.g03.models.*;
 import be.ac.ulb.infof307.g03.utils.Log;
+import be.ac.ulb.infof307.g03.utils.SplashWindow;
 import be.ac.ulb.infof307.g03.views.*;
 
 /**
@@ -29,6 +32,8 @@ public class Main {
 		
 		// first log
 		Log.debug("The program started");
+		
+		new SplashWindow(null, 15);
 		
 		// Mac OS X specific configuration
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
