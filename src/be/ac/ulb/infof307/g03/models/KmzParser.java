6c06e5c99db582ba0df2a592fb5e8fddc39dee7c
brochape <brochape@ulb.ac.be>
2015-02-08 16:27:00 +0100

    [add] Added the file for the kmz parser

---
+++ src/be/ac/ulb/infof307/g03/models/KmzParser.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package be.ac.ulb.infof307.g03.models;
+
+import java.awt.List;
+/**
+ * @author Bruno
+ *
+ */
+public class KmzParser extends Parser {
+
+	/* (non-Javadoc)
+	 * @see be.ac.ulb.infof307.g03.models.Parser#getVertices()
+	 */
+	@Override
+	public List getVertices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see be.ac.ulb.infof307.g03.models.Parser#getNormals()
+	 */
+	@Override
+	public List getNormals() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
