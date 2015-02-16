440a3e72fe7b9f05554b43450e82a50db40b78b4
Titouan Christophe <titouan.christophe@ulb.ac.be>
2015-02-16 15:32:39 +0100

    [ref] Parsers tests

---
+++ test/be/ac/ulb/infof307/g03/utils/parser/TestParsers.java
@@ -0,0 +1,11 @@
+package be.ac.ulb.infof307.g03.utils.parser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Test3DSParser.class, TestDaeParser.class, TestObjParser.class })
+public class TestParsers {
+
+}
