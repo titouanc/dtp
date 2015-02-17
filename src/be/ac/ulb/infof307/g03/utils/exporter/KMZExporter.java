b76baca48e1b82eb156f4993e126799421cb191d
Walter Moulart <wmoulart@ulb.ac.be>
2015-02-17 12:45:11 +0100

    [add] add a file in a zip for the kmz exporter

--- src/be/ac/ulb/infof307/g03/utils/exporter/KMZExporter.java
+++ src/be/ac/ulb/infof307/g03/utils/exporter/KMZExporter.java
@@ -1,12 +1,18 @@
 package be.ac.ulb.infof307.g03.utils.exporter;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import be.ac.ulb.infof307.g03.models.Entity;
 
 
 /**
- * @author pierre
+ * @author pierre,walter
  *
  */
 public class KMZExporter {
@@ -26,4 +32,55 @@ public class KMZExporter {
 	public void export(File fileToExport, Entity entity){
 
 	}
+	
+	/**
+	 * Add the kml/ DAE file to the zip
+	 * @param fileToExport
+	 */
+	public void addFileToJar(File fileToExport){
+		try {
+			FileOutputStream toBeExported = new FileOutputStream("exportedKMZ.kmz");
+			ZipOutputStream zip = new ZipOutputStream(toBeExported);
+
+			String file1Name = "exportedObject.kml";
+			addToZipFile(file1Name, zip);
+
+			zip.close();
+			toBeExported.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+
+
+	/**
+	 * Add a file to a ZIP
+	 * @param fileName
+	 * @param zos
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static void addToZipFile(String fileName, ZipOutputStream zip) throws FileNotFoundException, IOException {
+
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		ZipEntry zipEntry = new ZipEntry(fileName);
+		zip.putNextEntry(zipEntry);
+	
+		byte[] bytes = new byte[1024];
+		int length;
+		while ((length = fis.read(bytes)) >= 0) {
+			zip.write(bytes, 0, length);
+		}
+	
+		zip.closeEntry();
+		fis.close();
+	}
 }
+
+
