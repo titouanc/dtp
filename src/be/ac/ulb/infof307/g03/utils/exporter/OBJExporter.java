514ee28f4f81c8323188897af6e0ba5fa9fb637a
Titouan Christophe <titouan.christophe@ulb.ac.be>
2015-02-18 17:45:24 +0100

    [fix] Obj export for multiple primitives

--- src/be/ac/ulb/infof307/g03/utils/exporter/OBJExporter.java
+++ src/be/ac/ulb/infof307/g03/utils/exporter/OBJExporter.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import be.ac.ulb.infof307.g03.models.Entity;
@@ -32,32 +34,38 @@ public class OBJExporter {
 	public void export(File fileToExport, Entity entity){
 		try {
 			PrintWriter file = new PrintWriter(fileToExport,"UTF-8");
-			file.print("# Created with HomePlans");
-			file.println();
-			file.print("# www.supayr.ninja");
-			file.println();
-			for (Primitive primitive : entity.getPrimitives()) {
-				//Vectrices
+			file.println("# Created with HomePlans");
+			file.println("# www.supayr.ninja");
+			
+			// global vertex index
+			List<Primitive> allPrimitives = new ArrayList<Primitive>(entity.getPrimitives());
+			for (Primitive primitive : allPrimitives) {
+				//Vertices
 				float[] vertices = primitive.getVertices();
 				int verticesNumber = vertices.length;
 				for (int i=0; i<verticesNumber; i+=3) {
 					file.print("v ");
-					file.print(String.valueOf(vertices[i])+" ");
-					file.print(String.valueOf(vertices[i+1])+" ");
-					file.print(String.valueOf(vertices[i+2])+" ");
+					for (int j=0; j<3; j++)
+						file.print(String.valueOf(vertices[i+j])+" ");
 					file.println();
 				}
 				file.println();
+			}
+			
+			int vIndex = 0;
+			for (Primitive primitive : allPrimitives){
 				//Faces
 				int[] faces = primitive.getIndexes();
 				int facesNumber = faces.length;
 				for (int i=0; i<facesNumber; i+=3) {
 					file.print("f ");
-					file.print(String.valueOf(faces[i]+1)+" ");
-					file.print(String.valueOf(faces[i+1]+1)+" ");
-					file.print(String.valueOf(faces[i+2]+1)+" ");
+					for (int j=0; j<3; j++)
+						file.print(String.valueOf(vIndex + faces[i+j] + 1) + " ");
 					file.println();
-				}			
+				}
+				file.println();
+				
+				vIndex += primitive.getVertices().length/3;
 			}
 			file.close();
 		} catch (FileNotFoundException e) {
