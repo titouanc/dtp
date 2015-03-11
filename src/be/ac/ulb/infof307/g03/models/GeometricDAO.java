f707421551075bb5bd6c238dd3a98bb111854dc0
Titouan Christophe <titouan.christophe@ulb.ac.be>
2015-03-11 21:25:32 +0100

    [add] GeometricDAO Javadoc

--- src/be/ac/ulb/infof307/g03/models/GeometricDAO.java
+++ src/be/ac/ulb/infof307/g03/models/GeometricDAO.java
@@ -31,18 +31,39 @@ public class GeometricDAO<Batman extends Geometric> extends BaseDaoImpl<Batman, 
 		this.master = master;
 	}
 	
+	/**
+	 * Insert an object in the database, and notify the master of the
+	 * creation for subsequent notifyObservers() invocations
+	 * @param nanana The object to insert
+	 * @return The number of affected rows
+	 * @throws SQLException
+	 */
 	public int insert(Object nanana) throws SQLException {
 		int res = super.create((Batman) nanana);
 		this.master.addChange(Change.create((Batman) nanana));
 		return res;
 	}
 	
+	/**
+	 * Update an object in the database, and notify the master of the
+	 * modifications for subsequent notifyObservers() invocations
+	 * @param nanana The object to modify
+	 * @return The number of affected rows
+	 * @throws SQLException
+	 */
 	public int modify(Object nanana) throws SQLException {
 		int res = super.update((Batman) nanana);
 		this.master.addChange(Change.update((Batman) nanana));
 		return res;
 	}
 	
+	/**
+	 * Remove an object from the database, and notify the master of the
+	 * deletion for subsequent MasterDAO.notifyObservers() invocations
+	 * @param nanana The object to remove
+	 * @return The number of affected rows
+	 * @throws SQLException
+	 */
 	public int remove(Object nanana) throws SQLException {
 		int res = super.delete((Batman) nanana);
 		this.master.addChange(Change.delete((Batman) nanana));
